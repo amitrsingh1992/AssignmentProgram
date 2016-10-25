eeeab47e46526bfd7efec07568d0c6b12a789464
Amit R Singh <singh.amit.10ee1074@gmail.com>
2016-10-25 16:39:44 +0530

    Anagram

--- Anagram.java
+++ Anagram.java
@@ -1,64 +1,82 @@
+/*
+ * Anagram Trial Program
+ */
 import java.util.Scanner;
 
-public class Anagram
-{
-public String  removespace(String name )
-{
-int j=0;
-char[] l1;
-String S;
-l1 = name.toCharArray();
-char[] newname;
-for(int i=0;i<l1.size;i++)
-{
-if(l1[i]!=" ")
-{
+@SuppressWarnings("resource")
+public class Anagram {
+	public static boolean isAnagram(String s1, String s2) {
+		String A1, A2;
+		System.out.println("The space removal will be done from here:-");
+		A1 = removespace(s1);
+		System.out.println(A1);
+		A1 = A1.toUpperCase();
+		A2 = removespace(s2);
+		System.out.println(A2);
+		A2 = A2.toUpperCase();
+		System.out.println("The Arranging will be done from here:-");
+		A1 = arrange(A1);
+		System.out.println(A1);
+		A2 = arrange(A2);
+		System.out.println(A2);
+		if (A1.equals(A2) == true) {
+			return true;
+		} else
+			return false;
+	}
 
-newname[j]=l1[i];
-j++;
-}
-else 
-i+c String  removespace(String name )
-{
-int j=0;
-char[] l1;
-String S;
-l1 = name.toCharArray();
-char[] newname;
-for(int i=0;i<l1.size;i++)
-{
-if(l1[i]!=" ")
-{
+	public static String arrange(String Arr) {
+		char temp;
+		char[] A;
+		A = Arr.toCharArray();
+		System.out.println(A);
+		for (int i = 0; i < A.length - 1; i++) {
+			for (int j = i + 1; j < A.length; j++) {
+				if (A[i] > A[j]) {
+					temp = A[i];
+					A[i] = A[j];
+					A[j] = temp;
+				}
+			}
+		}
+		String T = new String(A);
+		System.out.println(T);
+		return T;
+	}
 
-newname[j]=l1[i];
-j++;
-}
-else
-i++;
+	public static String removespace(String name) {
+		int j = 0;
+		char[] l1;
+		String S;
+		l1 = name.toCharArray();
+		char[] newname = new char[50];
+		for (int i = 0; i < l1.length; i++) {
+			if (l1[i] != ' ') {
+				newname[j] = l1[i];
+				j++;
+			}
+		}
+		S = String.valueOf(newname);
+		return S;
+	}
 
-+;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1, s2;
+		boolean i;
 
-}
-S=newname.toString();
-return S;
-}
-public static void main(String[] args)
-{
+		Scanner n = new Scanner(System.in);
 
-String s1,s2;
-Scanner user_input = new Scanner(System.in);
+		System.out.print("Enter first String:");
+		s1 = n.nextLine();
+		System.out.print("Enter Second String:");
+		s2 = n.nextLine();
+		i = isAnagram(s1, s2);
+		if (i == true)
+			System.out.println("The entered strings are anagram. ");
+		else
+			System.out.println("The entered strings are not anagram.");
 
-System.out.println("Enter first String:");
-s1 = user_input.nextLine();
-System.out.println("Enter Second String:");
-s2 = user_input.nextLine();
-s1 = removespace(s1);
-System.out.println("String 1 = " +s1 );
-System.out.println("String 2 = " +s2);
-/*if(s1.length==s2.length)
-{
-
-}*/
-}
+	}
 
 }
